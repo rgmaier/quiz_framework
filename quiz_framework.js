3414a15afce49b8bdbdbe35a1df80041d5520b65
rgmaier <robert.maier@protonmail.ch>
2016-08-11 20:07:35 +0200

    Fixed level function

--- quiz_framework.js
+++ quiz_framework.js
@@ -1,7 +1,23 @@
+//Questions per Level are set here, standard is 5
+var questionsLevel = 2;
+
+//Max. level is set here, standard is 5 (starts at 0)
+var maxLevel = 0;
 var currentLevel = 0;
-var questionsInCurrentLevel = [0,1];
+
+var questionsInCurrentLevel = [];
 var score = 0;
 var correctAnswer = "";
+
+$(document).ready(function(){
+	getQuestionOrder();
+});
+
+$(window).unload(function(){
+	clearScore();
+	window.location.replace("./#page_home");
+});
+
 
 function shuffleArray(array) {
 	for (var i = array.length - 1; i > 0; i--) {
@@ -12,18 +28,43 @@ function shuffleArray(array) {
 	}
 	return array;
 }
-function getNewQuestion(){
-	var i = Math.floor((Math.random()*2));
-	$.getJSON("questions.json",function (data) {
-		var answer = shuffleArray([data.level[currentLevel].questions[i].answer1,data.level[currentLevel].questions[i].answer2,data.level[currentLevel].questions[i].answer3,data.level[currentLevel].questions[i].answer4]);
-		correctAnswer = data.level[currentLevel].questions[i].correctAnswer;
-		$("#guess").attr("src",data.level[currentLevel].questions[i].url);
-		$("#a1").text(answer[0]);
-		$("#a2").text(answer[1]);
-		$("#a3").text(answer[2]);
-		$("#a4").text(answer[3]);
-	});
 
+function getQuestionOrder(){
+	for(var i = 0; questionsInCurrentLevel.length < questionsLevel; i++){
+				questionsInCurrentLevel.push(i);
+			}
+	shuffleArray(questionsInCurrentLevel);
+}
+
+function getNewQuestion(){
+	
+	if(currentLevel >= maxLevel && questionsInCurrentLevel.length ==0){
+		//For some reason, this does not work
+		clearScore();
+		window.location.replace("./#page_done");
+	}else{
+		if(questionsInCurrentLevel.length == 0){
+			currentLevel++;
+			getQuestionOrder();
+		}
+		
+		var i = questionsInCurrentLevel.pop();
+		
+		$.getJSON("questions.json",function (data) {
+			var answer = shuffleArray([data.level[currentLevel].questions[i].answer1,data.level[currentLevel].questions[i].answer2,data.level[currentLevel].questions[i].answer3,data.level[currentLevel].questions[i].answer4]);
+			
+			correctAnswer = data.level[currentLevel].questions[i].correctAnswer;
+			
+			$("#guess").attr("src",data.level[currentLevel].questions[i].url);
+			$("#a1").text(answer[0]);
+			$("#a2").text(answer[1]);
+			$("#a3").text(answer[2]);
+			$("#a4").text(answer[3]);
+			$("#answerCheck").popup("close");
+		});
+		
+	}
+	return null;
 }
 
 function checkIfCorrect(answer){
@@ -38,10 +79,12 @@ function checkIfCorrect(answer){
 
 function clearScore(){
 	score = 0;
+	currentLevel = 0;
+	getQuestionOrder();
 }
 
 function setScore(){
-
+	$("#popup_score_text").text("Your score is "+ score + " and you are on on level " + (currentLevel+1)+".");
 }
 
 	
